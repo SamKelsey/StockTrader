193e7dfcaaa88b79c7ff94563918d42719a22892
Sam Kelsey <Sam.Kelsey@uk.ikm.com>
2020-08-25 21:06:06 +0100

    findStocks method

--- StockTrader.py
+++ StockTrader.py
@@ -1,1 +1,14 @@
-print("Hello")
+from bs4 import BeautifulSoup
+import requests
+
+def findStocks():
+    link = "https://finance.yahoo.com/most-active/"
+    r = requests.get(url = link)
+    soup = BeautifulSoup(r.content, 'lxml')
+    tableRows = soup.find('table', {'class': 'W(100%)'}).find('tbody').find_all('tr')
+    for row in tableRows:
+        ticker = row.find('td').find('a')
+        print(ticker.text)
+        
+
+findStocks()
