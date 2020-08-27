e691dd9249c36ba5a0bf44443544a5c443175415
Sam Kelsey <Sam.Kelsey@uk.ikm.com>
2020-08-27 21:17:35 +0100

    Add Stock class

---
+++ packages/Stock.py
@@ -0,0 +1,9 @@
+class Stock:
+    def __init__(self, symbol, flagLong, flagShort, qty):
+        self.symbol = symbol
+        self.flagLong = flagLong
+        self.flagShort = flagShort
+        self.qty = qty
+
+    
+    
--- packages/StockTrader.py
+++ packages/StockTrader.py
@@ -1,4 +1,7 @@
 from methods import findStocks, buyStock, sellStock, checkPositionQty, addToWatchlist, removeFromWatchlist, getWatchlistTickers
+from Stock import Stock
+
+
 
 
 
