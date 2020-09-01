298fdf5aae084a907bdf4ee972e8c4324d60b791
Sam Kelsey <Sam.Kelsey@uk.ikm.com>
2020-09-01 21:16:58 +0100

    createDF method

--- packages/methods/data_methods.py
+++ packages/methods/data_methods.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, json
+import requests, json, pandas as pd
 
 # Returns list of most active stock tickers from Yahoo Finance
 def findStocks():
@@ -14,7 +14,32 @@ def findStocks():
         tickersList.append(ticker)
     return tickersList
 
-# Accepts json response object and returns pandas df
+# Accepts json response object and returns dict of pandas dataframes by ticker name
 def createDF(r):
     response = json.loads(r.text)
-    print(response)
+
+    # Convert response object into dictionary of df's by ticker name
+    dfDict = {}
+    for ticker in response:
+        df = pd.DataFrame.from_dict(response[ticker])
+
+        # Convert time unit
+        df['t'] = pd.to_datetime(df['t'], unit='s')
+
+        # Set time as index and remove index name
+        df.set_index('t', inplace=True)
+        df.index.name = None
+
+        # Rename columns
+        df.rename(columns={
+            'o':'Open',
+            'h':'High',
+            'l':'Low',
+            'c':'Close',
+            'v':'Volume'
+            }, inplace=True)
+
+        dfDict[ticker] = df
+
+    return dfDict
+
