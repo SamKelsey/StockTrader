3fe48631ab8039404bb22b0e907838c1f37c3fa6
Sam Kelsey <sam.kelsey@bjss.com>
2020-09-14 21:38:20 +0100

    Updated main code

--- packages/StockTrader.py
+++ packages/StockTrader.py
@@ -1,23 +1,57 @@
 from Stock import Stock
 from methods import Alpaca_API_methods as API, data_methods as myData
 from datetime import datetime as dt
+from pathlib import Path
+import pandas as pd
+import os
+import time
 
-if dt.today().weekday() == 0: # Update watchlist every monday (TODO - Method of handling purchased stocks that need removed)
+if dt.today().weekday() == 0:  # Update watchlist every monday (TODO - Method of handling purchased stocks that need removed)
     # Get most volatile stocks and add them to Alpaca watchlist
     tickers = myData.findStocks()
     watchlistTickers = API.getWatchlistTickers()
     for ticker in tickers:
-        if ticker in watchlistTickers: # Check if ticker already in watchlist
+        if ticker in watchlistTickers:  # Check if ticker already in watchlist
             continue
         else:
-            API.addToWatchlist(ticker) # Add ticker to watchlist
+            API.addToWatchlist(ticker)  # Add ticker to watchlist
 
-myData.updateTickerData(['AMZN'])
+# Example list of tickers and quantity to buy/sell each time
+tickers = ['AMZN', 'TSLA']
+QTY = 1
+INTERVAL = 5
 
-# Update csv file info
-# myData.createDataFiles(myData.createDF(API.getTickerInfo(['AMZN', 'TSLA'], 3)))
+# Perform GET request for all tickers
+response = API.getTickerInfo(tickers, 10)
 
+# Convert GET response into dictionary of df's
+dfDict = myData.createDF(response)
 
+# Write df's to individual ticker csv files in ticker_data folder
+myData.createDataFiles(dfDict)
 
+# ENTER WHILE LOOP HERE !!!!!
 
+cwd = os.getcwd()  # Current Working Directory
 
+# Update ticker's csv files
+myData.updateTickerData(tickers)
+
+# Iterate through list of currently watched tickers
+for ticker in tickers:
+
+    # Read ticker csv file into a df
+    relativePath = "/packages/methods/ticker_data/"
+    pathString = cwd + relativePath + ticker + ".csv"
+    path = Path(pathString)
+    tickerDf = pd.read_csv(path)
+
+    # Determine whether to buy/sell latest index row of ticker df
+    result = myData.buyOrSell(ticker, tickerDf, -1)
+
+    if result == 1:  # Buy stocks of ticker
+        API.buyStock(ticker, QTY)
+    elif result == 2:  # Sell stocks of ticker
+        API.sellStock(ticker, QTY)
+
+time.sleep(60*INTERVAL)
--- packages/methods/data_methods.py
+++ packages/methods/data_methods.py
@@ -1,17 +1,49 @@
 from bs4 import BeautifulSoup
-import requests, json, pandas as pd, os
+import requests
+import json
+import pandas as pd
+import os
 from pathlib import Path
 from datetime import datetime
 from methods import Alpaca_API_methods as API
 from methods.API_info import LIMIT
 
 
+# Determines to buy or sell for a specified row index of a ticker df
+# Accepts a ticker, df and df index. Returns 1->Buy, 2->Sell, 0->No action
+def buyOrSell(ticker, df, i):
+    if df['Middle'].iloc[i] < df['Long'].iloc[i] and df['Short'].iloc[i] < df['Middle'].iloc[i] and df['LongChange'].iloc[i] > 0 and df['flagLong'].iloc[i] == False and df['flagShort'].iloc[i] == False:
+        df['flagShort'].iloc[i] = True
+        return 1  # BUY
+    elif df['flagShort'].iloc[i] == True and df['Short'].iloc[i] > df['Middle'].iloc[i]:
+        df['flagShort'].iloc[i] = False
+        return 2  # SELL
+    elif df['Middle'].iloc[i] > df['Long'].iloc[i] and df['Short'].iloc[i] > df['Middle'].iloc[i] and df['flagLong'].iloc[i] == False and df['flagShort'].iloc[i] == False:
+        df['flagLong'].iloc[i] = True
+        return 1  # BUY
+    elif df['flagLong'].iloc[i] == True and df['Short'].iloc[i] < df['Middle'].iloc[i]:
+        df['flagLong'].iloc[i] = False
+        return 2  # SELL
+    else:
+        # Carry forward previous flag if no action is required
+        if i != 0:
+            df['flagLong'].iloc[i] = df['flagLong'].iloc[i-1]
+            df['flagShort'].iloc[i] = df['flagShort'].iloc[i-1]
+        else:  # If it's the first index just set it to false
+            df['flagLong'] = False
+            df['flagShort'] = False
+
+        return 0  # NO ACTION
+
 # Returns list of most active stock tickers from Yahoo Finance
+
+
 def findStocks():
     link = "https://finance.yahoo.com/most-active/"
-    r = requests.get(url = link)
+    r = requests.get(url=link)
     soup = BeautifulSoup(r.content, 'lxml')
-    tableRows = soup.find('table', {'class': 'W(100%)'}).find('tbody').find_all('tr')
+    tableRows = soup.find('table', {'class': 'W(100%)'}).find(
+        'tbody').find_all('tr')
     tickersList = []
 
     for row in tableRows:
@@ -19,7 +51,10 @@ def findStocks():
         tickersList.append(ticker)
     return tickersList
 
+
 # Accepts json response object and returns dict of pandas dataframes by ticker name
+
+
 def createDF(r):
     response = json.loads(r.text)
 
@@ -35,37 +70,41 @@ def createDF(r):
         df.set_index('t', inplace=True)
         df.index.name = None
 
-        df['flagShort'] = False
         df['flagLong'] = False
+        df['flagShort'] = False
 
         # Rename columns
         df.rename(columns={
-            'o':'Open',
-            'h':'High',
-            'l':'Low',
-            'c':'Close',
-            'v':'Volume'
-            }, inplace=True)
-
+            'o': 'Open',
+            'h': 'High',
+            'l': 'Low',
+            'c': 'Close',
+            'v': 'Volume'
+        }, inplace=True)
 
         # Calculate short, medium and long exponential moving averages
-        shortSpan = LIMIT/200 
+        shortSpan = LIMIT/200
         middleSpan = LIMIT/10
         longSpan = LIMIT/2
-        ShortEMA = df.Close.ewm(span=shortSpan, adjust=False).mean() # span=5 is the original
-        MiddleEMA = df.Close.ewm(span=middleSpan, adjust=False).mean() #span=21
-        LongEMA = df.Close.ewm(span=longSpan, adjust=False).mean() # span=63
+        # span=5 is the original
+        ShortEMA = df.Close.ewm(span=shortSpan, adjust=False).mean()
+        MiddleEMA = df.Close.ewm(
+            span=middleSpan, adjust=False).mean()  # span=21
+        LongEMA = df.Close.ewm(span=longSpan, adjust=False).mean()  # span=63
 
         # Add exponential moving averages to df
         df['Short'] = ShortEMA
         df['Middle'] = MiddleEMA
         df['Long'] = LongEMA
-        
+        df['LongChange'] = df['Long'].pct_change()
+
         dfDict[ticker] = df
 
     return dfDict
 
 # Accepts dictionary of dataframes and converts each to a csv named by ticker
+
+
 def createDataFiles(dfDict):
     for ticker in dfDict:
         df = dfDict[ticker]
@@ -76,13 +115,15 @@ def createDataFiles(dfDict):
         df.to_csv(path)
 
 # Accepts list of tickers and updates the csv files with most recent info and removes most outdated info
+
+
 def updateTickerData(tickers):
     r = API.getTickerInfo(tickers, 1)
     response = json.loads(r.content)
     for ticker in response:
         df = pd.DataFrame.from_dict(response[ticker])
 
-         # Convert time unit
+        # Convert time unit
         df['t'] = pd.to_datetime(df['t'], unit='s')
 
         # Set time as index and remove index name
@@ -94,18 +135,18 @@ def updateTickerData(tickers):
 
         # Rename columns
         df.rename(columns={
-            'o':'Open',
-            'h':'High',
-            'l':'Low',
-            'c':'Close',
-            'v':'Volume'
-            }, inplace=True)
+            'o': 'Open',
+            'h': 'High',
+            'l': 'Low',
+            'c': 'Close',
+            'v': 'Volume'
+        }, inplace=True)
 
         # Read original csv
         cwd = os.getcwd()
         path = Path(cwd + "/packages/methods/ticker_data/" + ticker + ".csv")
         dfOld = pd.read_csv(path, index_col=0)
-        
+
         # Check for duplicate index's
         indexString1 = str(df.index[0])
         indexString2 = dfOld.index[-1]
@@ -113,37 +154,28 @@ def updateTickerData(tickers):
         if indexString1 == indexString2:
             continue
 
-        # Add new info to end 
+        # Add new info to end
         dfNew = dfOld.append(df)
 
         # Remove oldest info (first row)
-        dfNew = dfNew.iloc[1:,]
+        dfNew = dfNew.iloc[1:, ]
 
         # Calculate short, medium and long exponential moving averages
-        shortSpan = LIMIT/200 
+        shortSpan = LIMIT/200
         middleSpan = LIMIT/10
         longSpan = LIMIT/2
-        ShortEMA = dfNew.Close.ewm(span=shortSpan, adjust=False).mean() # span=5 is the original
-        MiddleEMA = dfNew.Close.ewm(span=middleSpan, adjust=False).mean() #span=21
-        LongEMA = dfNew.Close.ewm(span=longSpan, adjust=False).mean() # span=63
+        # span=5 is the original
+        ShortEMA = dfNew.Close.ewm(span=shortSpan, adjust=False).mean()
+        MiddleEMA = dfNew.Close.ewm(
+            span=middleSpan, adjust=False).mean()  # span=21
+        LongEMA = dfNew.Close.ewm(
+            span=longSpan, adjust=False).mean()  # span=63
 
         # Add exponential moving averages to df
         dfNew['Short'] = ShortEMA
         dfNew['Middle'] = MiddleEMA
         dfNew['Long'] = LongEMA
+        df['LongChange'] = df['Long'].pct_change()
 
-        print(dfNew)
         # Write to csv file under same name
         dfNew.to_csv(path)
-            
-
-
-        
-
-
-    
-
-        
-
-
-    
