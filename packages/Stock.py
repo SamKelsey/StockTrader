18f03c82a5c8fe5d8b0546227f4739a36aabf961
Sam Kelsey <sam.kelsey@bjss.com>
2020-09-14 21:38:54 +0100

    Delete Stock class

--- packages/Stock.py
+++
@@ -1,9 +0,0 @@
-class Stock:
-    def __init__(self, symbol, flagLong, flagShort, qty):
-        self.symbol = symbol
-        self.flagLong = flagLong
-        self.flagShort = flagShort
-        self.qty = qty
-
-    
-    
